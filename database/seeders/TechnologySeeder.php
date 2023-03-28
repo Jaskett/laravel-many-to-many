eb77ea930bf748092a7c7fc186a33d5af0cca34e
Brandon Vasquez <thatsjaskett@gmail.com>
2023-03-28 16:05:15 +0200

    Creato Model, Migration e Seeder di Technology

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
+// Helpers
+use Illuminate\Support\Str;
 
 class TechnologySeeder extends Seeder
 {
@@ -15,5 +19,24 @@ class TechnologySeeder extends Seeder
     public function run()
     {
         //
+
+        $technologies = [
+            'HTML',
+            'CSS',
+            'Bootstrap',
+            'Javascript',
+            'Vue',
+            'Vite',
+            'SASS',
+            'PHP',
+            'Laravel'
+        ];
+
+        foreach ($technologies as $technology) {
+            $newTechnology = Technology::create([
+                'name'=>$technology,
+                'slug'=>Str::slug($technology)
+            ]);
+        }
     }
 }
